559a82322778f63c9d45e5643f3698a273f62178
ItzikDevio <itzikdevio@gmail.com>
2022-03-04 17:53:06 +0200

    [FIX] Added declration for shared-functions and more WIP

--- apps/app-client/pages/index.tsx
+++ apps/app-client/pages/index.tsx
@@ -1,6 +1,10 @@
 import type { NextPage } from "next";
+import { general } from "@app/shared-functions";
 
 const Home: NextPage = () => {
+  const result = general.test();
+  console.log(result);
+
   return <div>Hello world</div>;
 };
 
