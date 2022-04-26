2cb397bafc8a8edc4d2bb03fcc797337a96ff3c9
Itzikgabay <itzikdevio@gmail.com>
2022-04-26 23:14:48 +0300

    [FIX] fixed pm2 configuration

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,26 +1,28 @@
 module.exports = {
   apps: [
     {
-      name: "client",
-      script: "./node_modules",
+      name: "app",
+      script: "cd ./apps/app-website && yarn dev",
+      env: {
+        NODE_ENV: "development",
+      },
       env_production: {
         NODE_ENV: "production",
       },
-      env_development: {
-        NODE_ENV: "development",
-      },
       watch: true,
     },
-    // {
-    //   name: "./apps/app-server",
-    //   script: "./apps",
-    //   env_production: {
-    //     NODE_ENV: "production",
-    //   },
-    //   env_development: {
-    //     NODE_ENV: "development",
-    //   },
-    //   watch: true,
-    // },
   ],
+
+  deploy: {
+    production: {
+      user: "SSH_USERNAME",
+      host: "SSH_HOSTMACHINE",
+      ref: "origin/master",
+      repo: "GIT_REPOSITORY",
+      path: "DESTINATION_PATH",
+      "pre-deploy-local": "",
+      "post-deploy": "npm install && pm2 reload ecosystem.config.js --env production",
+      "pre-setup": "",
+    },
+  },
 };
